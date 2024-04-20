ba5c5a4a59adb6da5e18a74cd2864773030c56a2
LAPTOP-HUARQIE4\kenshi <princesskenshi73@gmail.com>
2024-04-21 00:42:56 +0800

    yaur

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const know = document.querySelector('#know');
+    const header = document.querySelector('.header');
     const info = document.querySelector('.info');
 
-    know.addEventListener('click', function () {
+    header.addEventListener('click', function () {
         const margin = 25; // Adjust the margin value as needed
         const infoOffset = info.offsetTop - margin;
         window.scrollTo({ top: infoOffset, behavior: 'smooth' });
